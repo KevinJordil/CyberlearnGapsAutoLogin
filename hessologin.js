ef0138d95be01a6aad8579c28d343f54f891fe47
KevinJordil <kevin.jordil@heig-vd.ch>
2022-02-23 14:20:31 +0100

    Add icon

--- hessologin.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("username").value = ""
-document.getElementById("password").value = ""
-
-document.getElementById("login-button").click()
---
+++ js/hessologin.js
@@ -0,0 +1,7 @@
+if(document.getElementById("username") != null){
+    document.getElementById("username").value = ""
+    document.getElementById("password").value = ""
+
+    document.getElementById("login-button").click()
+}
+
