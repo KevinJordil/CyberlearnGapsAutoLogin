973c59fa90b6819addbbe7d5e061ac65e91a23bf
Kevin Jordil <kevin.jordil@outlook.com>
2022-02-25 23:04:30 +0100

    password show switch

--- js/popup.js
+++ js/popup.js
@@ -1,12 +1,15 @@
 document.addEventListener('DOMContentLoaded', function() {
-    let save_button = document.getElementById("save_button")
-    let email = document.getElementById("input-email")
-    let password = document.getElementById("input-password")
+    window.resizeBy(800, 600)
+
+    let save_button = document.getElementById("save-button")
+    let input_email = document.getElementById("input-email")
+    let input_password = document.getElementById("input-password")
+    let show_password = document.getElementById("show-password")
 
     chrome.storage.sync.get("user", function(result) {
         if(result.user !== undefined){
-            email.value = result.user.email
-            password.value = result.user.password
+            input_email.value = result.user.email
+            input_password.value = result.user.password
         } 
     })
 
@@ -16,19 +19,36 @@ document.addEventListener('DOMContentLoaded', function() {
         }
     })
 
-
     save_button.addEventListener("click", function() {
 
         chrome.storage.sync.set({ 
             "user" : {
-                "email": email.value,
-                "password": password.value
+                "email": input_email.value,
+                "password": input_password.value
             }
         }, function() {
             save_button.classList.remove("is-link")
-            save_button.innerText = "Saved !"
+            save_button.innerText = "Sauvé !"
             save_button.classList.add("is-success")
         })
 
     }, false)
+
+
+    show_password.addEventListener("click", function() {
+
+        if(input_password.type == "password"){
+            input_password.type = "text"
+            show_password.classList.remove("fa-eye")
+            show_password.classList.add("fa-eye-slash")
+        }
+        else if(input_password.type == "text"){
+            input_password.type = "password"
+            show_password.classList.remove("fa-eye-slash")
+            show_password.classList.add("fa-eye")
+        }
+
+    }, false)
+
+
 }, false)
