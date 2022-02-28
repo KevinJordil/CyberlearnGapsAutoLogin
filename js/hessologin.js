c97bbd6fecdd27e7da8ca5b32589d8d420e584df
KevinJordil <kevin.jordil@heig-vd.ch>
2022-02-28 10:56:18 +0100

    Fix spam login failed

--- js/hessologin.js
+++ js/hessologin.js
@@ -1,10 +1,19 @@
 if(document.getElementById("username") != null){
-    chrome.storage.sync.get("user", function(result) {
-        if(result.user !== undefined){
-            document.getElementById("username").value = result.user.email
-            document.getElementById("password").value = result.user.password
-        
-            document.getElementById("login-button").click()
-        } 
-    })
+
+    let error_message = document.getElementsByClassName("input_error")[0]
+
+    if(error_message === undefined){
+        chrome.storage.sync.get("user", function(result) {
+            if(result.user !== undefined){
+                document.getElementById("username").value = result.user.email
+                document.getElementById("password").value = result.user.password
+            
+                document.getElementById("login-button").click()
+            } 
+        })
+    } else { // Connection failed
+        error_message.innerHTML = "Les informations d'identification fournies dans l'extension sont erronées<br>Veuillez les mettre à jour et réssayer !"
+    }
+
+
 }
