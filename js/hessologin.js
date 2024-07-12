8edabbab0907ef01d2159f461afc6dc6be84f1fc
Kevin Jordil <kevin.jordil@outlook.com>
2024-07-12 13:51:12 +0200

    Update with new login page

--- js/hessologin.js
+++ js/hessologin.js
@@ -1,17 +1,17 @@
 if (document.getElementById("username") != null) {
 
-    let error_message = document.getElementsByClassName("input_error")[0]
+    let error_message = document.getElementsByClassName("text-danger")[0]
 
     if (error_message === undefined) {
         chrome.storage.sync.get("user", function (result) {
             if (result.user !== undefined) {
                 if (document.getElementById("password") == null) { // First page with username only
                     document.getElementById("username").value = result.user.email
-                    document.getElementById("login-button").click()
+                    document.getElementById("button-submit").click()
                 } else { // Second page with username and password
                     document.getElementById("username").value = result.user.email
                     document.getElementById("password").value = result.user.password
-                    document.getElementById("login-button").click()
+                    document.getElementById("button-proceed").click()
                 }
 
 
